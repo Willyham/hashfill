8e8cfb5fd0ed5cdf80c60600bf597222beb25f64
Tomasz Rakowski <akayami@gmail.com>
2024-01-27 00:13:05 -0500

    Switched to engelsjk/polygol to avoid geos (#1) and upgraded deps

--- go.mod
+++ go.mod
@@ -1,16 +1,10 @@
 module github.com/Willyham/hashfill
 
-go 1.20
+go 1.12
 
 require (
+	github.com/engelsjk/polygol v0.0.3
 	github.com/mmcloughlin/geohash v0.10.0
-	github.com/paulsmith/gogeos v0.1.2
 	github.com/stretchr/testify v1.8.4
 	github.com/twpayne/go-geom v1.5.3
-)
-
-require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- predicates.go
+++ predicates.go
@@ -1,8 +1,8 @@
 package hashfill
 
 import (
+	"github.com/engelsjk/polygol"
 	"github.com/mmcloughlin/geohash"
-	"github.com/paulsmith/gogeos/geos"
 	geom "github.com/twpayne/go-geom"
 )
 
@@ -30,58 +30,78 @@ func (f intersectsFunc) Intersects(p *geom.Polygon, hash string) (bool, error) {
 	return f(p, hash)
 }
 
-// Intersects tests if the geofence contains the hash by doing a geos intersection.
+// Intersects tests if the geofence contains the hash.
 var Intersects = intersectsFunc(func(geofence *geom.Polygon, hash string) (bool, error) {
-	hashGeo := hashToGeometry(hash)
-	fence := polygonToGeometry(geofence)
-	return fence.Intersects(hashGeo)
+	hashGeom := hashToGeom(hash)
+	out, err := polygol.Intersection(hashGeom, g2p(geofence))
+	if err != nil {
+		return false, err
+	}
+	if len(out) == 0 {
+		return false, nil
+	} else {
+		return true, nil
+	}
 })
 
-// Contains tests if the geofence contains the hash by doing a geos contains.
+// Contains tests if the geofence contains the hash.
 var Contains = containsFunc(func(geofence *geom.Polygon, hash string) (bool, error) {
-	hashGeo := hashToGeometry(hash)
-	fence := polygonToGeometry(geofence)
-	return fence.Contains(hashGeo)
+	hashGeom := hashToGeom(hash)
+	fenceGeom := g2p(geofence)
+	out, err := polygol.Intersection(fenceGeom, hashGeom)
+	if err != nil {
+		return false, err
+	}
+	if len(out) == 0 {
+		return false, nil
+	} else {
+		o, err := polygol.XOR(hashGeom, out)
+		if err != nil {
+			return false, err
+		}
+		if len(o) == 0 {
+			return true, err
+		}
+		return false, nil
+	}
 })
 
-func geomToGeosCoord(coord geom.Coord) geos.Coord {
-	return geos.Coord{
-		X: coord.X(),
-		Y: coord.Y(),
-	}
-}
-
-func geomToGeosCoords(coords []geom.Coord) []geos.Coord {
-	out := make([]geos.Coord, len(coords))
-	for i := 0; i < len(coords); i++ {
-		out[i] = geomToGeosCoord(coords[i])
-	}
-	return out
-}
-
-// hashToGeometry converts a a geohash to a geos polygon by taking its bounding box.
-func hashToGeometry(hash string) *geos.Geometry {
+func hashToGeom(hash string) polygol.Geom {
 	bounds := geohash.BoundingBox(hash)
-	return geos.Must(geos.NewPolygon([]geos.Coord{
-		geos.NewCoord(bounds.MinLng, bounds.MinLat),
-		geos.NewCoord(bounds.MinLng, bounds.MaxLat),
-		geos.NewCoord(bounds.MaxLng, bounds.MaxLat),
-		geos.NewCoord(bounds.MaxLng, bounds.MinLat),
-		geos.NewCoord(bounds.MinLng, bounds.MinLat),
-	}))
+	return polygol.Geom{{{
+		{bounds.MinLng, bounds.MinLat},
+		{bounds.MinLng, bounds.MaxLat},
+		{bounds.MaxLng, bounds.MaxLat},
+		{bounds.MaxLng, bounds.MinLat},
+		{bounds.MinLng, bounds.MinLat},
+	}}}
 }
 
-func polygonToGeometry(geofence *geom.Polygon) *geos.Geometry {
-	// Convert the outer shell to geos format.
-	shell := geofence.LinearRing(0).Coords()
-	shellGeos := geomToGeosCoords(shell)
+func g2p(g geom.T) [][][][]float64 {
 
-	// Convert each hole to geos format.
-	numHoles := geofence.NumLinearRings() - 1
-	holes := make([][]geos.Coord, numHoles)
-	for i := 0; i < numHoles; i++ {
-		holes[i] = geomToGeosCoords(geofence.LinearRing(i).Coords())
+	var coords [][][]geom.Coord
+
+	switch v := g.(type) {
+	case *geom.Polygon:
+		coords = [][][]geom.Coord{v.Coords()}
+	case *geom.MultiPolygon:
+		coords = v.Coords()
 	}
 
-	return geos.Must(geos.NewPolygon(shellGeos, holes...))
+	p := make([][][][]float64, len(coords))
+
+	for i := range coords {
+		p[i] = make([][][]float64, len(coords[i]))
+		for j := range coords[i] {
+			p[i][j] = make([][]float64, len(coords[i][j]))
+			for k := range coords[i][j] {
+				coord := coords[i][j][k]
+				pt := make([]float64, 2)
+				pt[0], pt[1] = coord.X(), coord.Y()
+				p[i][j][k] = pt
+			}
+		}
+	}
+
+	return p
 }
